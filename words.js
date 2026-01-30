39f45bc6c59158749d9566fd88f50d42f0544495
Yiping GE <108039517+geyiping@users.noreply.github.com>
2026-01-30 20:16:51 +0100

    Rename categories in wordGroups to Italian

--- words.js
+++ words.js
@@ -1,23 +1,23 @@
 const wordGroups = {
-  "list1": [ // wateranimal
+  "lAnimali acquatici": [ // wateranimal
     "Acciuga","aragosta","Balena","branzino","Cefalo","cozza","Delfino","dentice","Eglefino","esocarpo",
     "Fagiano","fanghiglia","Gambero","gallinella","Halibut","icore","Merluzzo","muggine","Nasello","ombre",
     "Orata","ostrica","Pagello","polpo","Rombo","riccio","Salmone","sogliola","Tonno","triglia",
     "Anguilla","alosa","Cernia","chirurgo","Carpa","grongo","Sardina","scorfano","Tinca","Vongola"
   ],
-  "list2": [ // mammals
+  "Mammiferi": [ // mammals
     "Asino","armadillo","Bue","bisonte","Cane","capibara","Daino","Elefante","ermellino","Faina",
     "furetto","Gatto","giraffa","Ippopotamo","irbis","Leone","lupo","Maiale","cervo","Orso",
     "ocelot","Pecora","puma","Quokka","ratto","Scoiattolo","serval","Tigre","topo","Volpe",
     "vombato","Zebra","zibellino","Canguro","ghiro","Lemure","linx","Tasso","dromedario","Dingo"
   ],
-  "list3": [ // birds
+  "Uccelli": [ // birds
     "Airone","anatra","Cardinale","cinciallegra","Colombo","cormorano","Falco","fringuello","Gabbiano","gazza",
     "Ibis","passero","Pappagallo","Rondine","ricciolo","Storno","struzzo","Tacchino","tordo","Usignolo",
     "uccello","Verdone","zigolo","Oca","piviere","picchio","Ghiandaia","gallina","Cardellino","gufo",
     "Pernice","pavone","Allodola","merlo","Aquila","Marangone","anitra","Fagiano","beccaccia","garzetta"
   ],
-  "list4": [ // insectsetc
+  "Insetti": [ // insectsetc
     "Formica","farfalla","Cimice","cavalletta","Grillo","gerride","Libellula","scarabeo","scarafaggio","Zanzara",
     "zecca","Coccinella","mosca","Vespa","verme","Termite","tarma","Bombice","baco","Mantide",
     "Farfallina","formicaleone","Moscerino","cimicep","Tarantola","scorpione","ragno","opilione","aracnide","pidocchio",
